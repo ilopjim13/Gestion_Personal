1bcfc74764862c58cab0f0ec0db5420c5b359c1d
ilopjim13 <ljivan@iesrafaelalberti.es>
2024-02-01 21:33:29 +0100

    ejercicio empezado en clase

--- src/main/kotlin/Empleado.kt
+++ src/main/kotlin/Empleado.kt
@@ -1,5 +1,15 @@
-class Empleado(nombre:String, edad:Int, var salarioBase: Double, var porImpuestos: Double = 10.0) :Persona(nombre, edad) {
+open class Empleado(nombre:String, edad:Int, var salarioBase: Double, var porImpuestos: Double = 10.0) :Persona(nombre, edad) {
 
+    open fun calcularSalario():Double {
+        return salarioBase * (porImpuestos / 100)
+    }
 
+    fun chamba():String {
+        return "$nombre está trabajando en la empresa"
+    }
+
+    override fun toString(): String {
+        return "${super.toString()}, Salario Base: $salarioBase€"
+    }
 
 }
--- src/main/kotlin/Gerente.kt
+++ src/main/kotlin/Gerente.kt
@@ -1,2 +1,8 @@
-class Gerente {
+class Gerente(nombre:String, edad:Int, salarioBase, porImpuestos:Double, var bonus:Double, var excentoImpuestos: Boolean = false) :Empleado(nombre, edad, salalrioBase, porImpuestos) {
+
+    override fun calcularSalario() :Double {
+        if (excentoImpuestos) {
+
+        }
+    }
 }
